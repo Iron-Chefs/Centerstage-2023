04323d9962dff3c55d6fc68996b2a08cd2c3ab0c
im me <149269373+blargin@users.noreply.github.com>
2023-10-30 16:24:56 -0400

    gave birth to ramsay, kidnapped some code

---
+++ TeamCode/src/main/java/org/Ramsay.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class Ramsay {
+    //drivetrain
+    DcMotor frontLeftMotor;
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
+
+//lift
+DcMotor liftMotor1;
+DcMotor liftMotor2;
+
+//intake
+DcMotor intakeMotor;
+
+//Grabber
+
+Servo pivotServo;
+Servo grabberServo;
+
+    public Ramsay() {
+        frontLeftMotor = hardwareMap.dcMotor.get("fl");
+        backLeftMotor = hardwareMap.dcMotor.get("bl");
+        frontRightMotor = hardwareMap.dcMotor.get("fr");
+        backRightMotor = hardwareMap.dcMotor.get("br");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //lift init
+        liftMotor1 = hardwareMap.dcMotor.get("lift1");
+        liftMotor2 = hardwareMap.dcMotor.get("lift2");
+        //intake init
+        intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
+        //grabber init
+        pivotServo = hardwareMap.servo.get("pivot");
+        grabberServo = hardwareMap.servo.get("grabber");
+
+    }
+    public void drive(double straight, double strafe, double turn) {
+            double denominator = Math.max(Math.abs(straight) + Math.abs(strafe) + Math.abs(turn), 1);
+            double frontLeftPower = (straight + strafe + turn) / denominator;
+            double backLeftPower = (straight - strafe + turn) / denominator;
+            double frontRightPower = (straight - strafe - turn) / denominator;
+            double backRightPower = (straight + strafe - turn) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+    }
+    public void closeGrabber() {
+        grabberServo.setPosition(0);
+    }
+    public void openGrabber() {
+        grabberservo.setPosition(1);
+    }
+    
+    public void lift(double power) {
+    liftMotor1.setPower(power);
+    liftMotor2.setPower(power);
+    }
+}
